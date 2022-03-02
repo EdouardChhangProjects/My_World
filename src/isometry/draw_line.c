7afa33935a09e76e9bfc4017f76a95ea7fa82c98
EpiDoubleHaxe <edouard.chhang@epitech.eu>
2022-03-02 17:50:08 +0100

    --amend

--- src/isometry/draw_line.c
+++ src/isometry/draw_line.c
@@ -4,3 +4,17 @@
 ** File description:
 ** draw_line
 */
+
+#include "my_world.h"
+
+sfVertexArray *create_line(sfVector2f *point1, sfVector2f* point2)
+{
+    sfVertexArray *vertex_array = sfVertexArray_create();
+    sfVertex vertex1 = {.position = * point1, .color = sfWhite};
+    sfVertex vertex2 = {.position = * point2, .color = sfWhite};
+
+    sfVertexArray_append(vertex_array, vertex1);
+    sfVertexArray_append(vertex_array, vertex2);
+    sfVertexArray_setPrimitiveType(vertex_array, sfLinesStrip);
+    return (vertex_array);
+}
