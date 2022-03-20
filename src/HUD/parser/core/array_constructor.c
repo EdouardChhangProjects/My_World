97b46fb2c2bf1362dd9a128ff9b5c7138df0db63
EpiDoubleHaxe <edouard.chhang@epitech.eu>
2022-03-20 17:53:49 +0100

    test coding style

--- src/HUD/parser/core/array_constructor.c
+++ src/HUD/parser/core/array_constructor.c
@@ -52,10 +52,10 @@ sfColor hud_parser_sfcolor(char *color_constr)
     int nbrs[] = {0, 0, 0, 0};
 
     if (arr == NULL)
-        return (sfColor){.r = -1, .g = -1, .b = -1, .a = -1};
+        return (sfColor) {.r = -1, .g = -1, .b = -1, .a = -1};
     for (int i = 0; i < 4; i++) {
         if (arr[i] == NULL)
-            return (sfColor){.r = -1, .g = -1, .b = -1, .a = -1};
+            return (sfColor) {.r = -1, .g = -1, .b = -1, .a = -1};
         nbrs[i] = my_getnbr(arr[i]);
     }
     rect = (sfColor) {
